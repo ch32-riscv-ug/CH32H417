ae853eb717be3aa639f822002c07b9c9d5e7e669
root <tanakamasayuki@gmail.com>
2026-04-14 14:49:23 +0900

    update

--- update.sh
+++ update.sh
@@ -13,13 +13,13 @@ cd ..
 # https://www.wch.cn/products/CH32H417.html
 cd datasheet_zh
 # https://www.wch.cn/downloads/CH32H417DS0_PDF.html
-curl -z CH32H417DS0.PDF -o CH32H417DS0.PDF https://www.wch.cn/download/file?id=529
+curl -z CH32H417DS0.PDF -o CH32H417DS0.PDF https://file.wch.cn/download/file?id=529
 # https://www.wch.cn/downloads/CH32H417RM_PDF.html
-curl -z CH32H417RM.PDF -o CH32H417RM.PDF https://www.wch.cn/download/file?id=532
+curl -z CH32H417RM.PDF -o CH32H417RM.PDF https://file.wch.cn/download/file?id=532
 cd ..
 
 # https://www.wch.cn/downloads/CH32H417EVT_ZIP.html
-curl -z CH32H417EVT.ZIP -o CH32H417EVT.ZIP https://www.wch.cn/download/file?id=530
+curl -z CH32H417EVT.ZIP -o CH32H417EVT.ZIP https://file.wch.cn/download/file?id=530
 rm -rfv EVT
 unzip -O GB2312 *.ZIP
 
